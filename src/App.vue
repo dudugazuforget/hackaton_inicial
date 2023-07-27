5385f8850df6acaa0437a980ebd360371c8d0ab8
dudugazuforget <dudu99744@gmail.com>
2023-07-27 09:04:41 -0300

    init: inicio hack

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {imoveis} from '@/_data/imoveis.js';
+
+</script>
+
+<template>
+  <h1>Casas que são a sua cara</h1>
+<div>
+  <div v-for="casa in imoveis">
+    <hr>
+    <p class="Casa-valor">{{ casa.valor }}</p>
+    <button>vende-se</button>
+    <img :src= 'casa.img'>
+  </div>
+</div>
+</template>
+
+<style scoped>
+img{
+  width: 300px;
+  height: 300px;
+  
+}
+button{
+  background-color: rgb(10, 43, 10);
+  color: white;
+  border-radius: 50px;
+
+}
+</style>
---
+++ src/_data/imoveis.js
@@ -0,0 +1,9 @@
+export const imoveis = [
+    {
+    id: 1,
+    valor: 450.000 ,
+    local: 'Rua Henrique Pererira, 2224 Bairro: Joaquim',    
+    img:'https://projetaronline.com/wp-content/uploads/2022/11/casa-pequena-planta-baixa-0.png'  
+},
+
+]
