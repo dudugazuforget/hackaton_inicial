755a46fb879e03f99d00feca4e7406a247202587
dudugazuforget <dudu99744@gmail.com>
2023-07-28 16:50:30 -0300

    listagem dos itens

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <script setup>
-import {imoveis} from '@/_data/imoveis.js';
+import { imoveis } from '@/_data/imoveis.js';
 
 </script>
 
 <template>
-  <h1>Casas que são a sua cara</h1>
-<div>
-  <div v-for="casa in imoveis">
-    <hr>
-    <p class="Casa-valor">{{ casa.valor }}</p>
-    <button>vende-se</button>
-    <img :src= 'casa.img'>
+  <h1>casas que são a sua cara</h1>
+
+
+  <div class="listagem-imoveis">
+    <div v-for="imovel in imoveis" :key="imovel.id" class="card-casa">
+      <hr>
+      <button>Vende-se</button>
+      <p class="imovel-valor">{{ imovel.valor }}</p>
+      <img :src="imovel.img" alt="capa" class="imovel-capa">
+      <p class="imovel-local">{{ imovel.local }}</p>
+      <hr>
+    </div>
   </div>
-</div>
 </template>
 
 <style scoped>
-img{
+.listagem-imoveis {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  margin-left: 100px;
+}
+
+.card-casa {
+  margin: 0 50px;
+}
+
+img {
   width: 300px;
   height: 300px;
-  
+
 }
-button{
+
+button {
   background-color: rgb(10, 43, 10);
   color: white;
   border-radius: 50px;
--- src/_data/imoveis.js
+++ src/_data/imoveis.js
@@ -5,5 +5,23 @@ export const imoveis = [
     local: 'Rua Henrique Pererira, 2224 Bairro: Joaquim',    
     img:'https://projetaronline.com/wp-content/uploads/2022/11/casa-pequena-planta-baixa-0.png'  
 },
+{
+    id: 1,
+    valor: 450.000 ,
+    local: 'Rua Henrique Pererira, 2224 Bairro: Joaquim',    
+    img:'https://projetaronline.com/wp-content/uploads/2022/11/casa-pequena-planta-baixa-0.png'  
+},
+{
+    id: 1,
+    valor: 450.000 ,
+    local: 'Rua Henrique Pererira, 2224 Bairro: Joaquim',    
+    img:'https://projetaronline.com/wp-content/uploads/2022/11/casa-pequena-planta-baixa-0.png'  
+},
+{
+    id: 1,
+    valor: 450.000 ,
+    local: 'Rua Henrique Pererira, 2224 Bairro: Joaquim',    
+    img:'https://projetaronline.com/wp-content/uploads/2022/11/casa-pequena-planta-baixa-0.png'  
+},
 
 ]
