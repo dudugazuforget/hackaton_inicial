ae5fc7bdcfa865d53c507f45adeedd264360936d
dudugazuforget <dudu99744@gmail.com>
2023-08-01 11:50:04 -0300

    atualização no card e navbar

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,25 @@
 import { imoveis } from '@/_data/imoveis.js';
 
 
+
 </script>
 
 <template>
   <div class="title">
-    <h1>Casas que são a sua cara</h1>
+    <h1 class="name-title">IfCoders</h1>
+    <button class="sobre-nos">Sobre nós</button>
+    <button class="contato">Contato</button>
+    <button class="anunciar">Anunciar</button>
+
   </div>
+  <div class="navbar"></div>
   <div class="listagem-imoveis">
     <div v-for="imovel in imoveis" :key="imovel.id" class="card-casa">
-      <hr>
-      <span><button>Vende-se</button></span>
-      <p class="imovel-valor">R${{ imovel.valor }}.000</p>
       <img :src="imovel.img" alt="capa" class="imovel-capa">
-      <p class="imovel-local">{{ imovel.local }}</p>
-  
       <hr>
+      <span><button class="b-alugar">Alugar</button></span>
+      <p class="imovel-valor">R${{ imovel.valor }}.000</p>
+      <p class="imovel-local">{{ imovel.local }}</p>
     </div>
   </div>
 </template>
@@ -32,8 +36,54 @@ import { imoveis } from '@/_data/imoveis.js';
 }
 
 .card-casa {
-  margin: 0 50px;
-  margin-top: 150px;
+  margin: 0 29px;
+  margin-top: 300px;
+  background-color: rgb(241, 235, 235);
+  border-radius: 20px;
+  width: 360px;
+
+}
+
+.anunciar {
+  position: absolute;
+  background-color: transparent;
+  color: rgb(200, 221, 200);
+  border: 0;
+  margin-top: -37px;
+  margin-left: 460px;
+}
+
+.contato {
+  position: absolute;
+  background-color: transparent;
+  color: rgb(200, 221, 200);
+  border: 0;
+
+  margin-top: -37px;
+  margin-left: 350px;
+}
+
+.sobre-nos {
+  position: absolute;
+  background-color: transparent;
+  color: rgb(200, 221, 200);
+  border: 0;
+  margin-top: -37px;
+  margin-left: 230px;
+}
+
+.imovel-capa {
+ 
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  width: 100%;
+
+}
+
+.name-title {
+  margin-top: 20px;
+  margin-left: 20px;
+  color: rgb(34, 161, 34);
 }
 
 .title {
@@ -44,13 +94,18 @@ import { imoveis } from '@/_data/imoveis.js';
   height: 10vh;
   border: 0;
   background-color: rgb(44, 40, 40);
-  text-align: center;
+  text-align: left;
   font-family: 'Poppins', sans-serif;
-  color: rgb(255, 131, 30);
+  color: rgb(200, 221, 200);
+  font-weight: bold;
+
 }
 
 .imovel-local {
-  color: rgb(252, 121, 14);
+  margin-top: -40px;
+  margin-bottom: 50px;
+  margin-left: 5px;
+  color: rgb(15, 9, 4);
   font-family: 'Poppins', sans-serif;
 }
 
@@ -60,7 +115,7 @@ img {
 
 }
 
-button {
+.b-alugar {
   position: relative;
   border: 0;
   background-color: rgb(19, 88, 19);
@@ -73,28 +128,12 @@ button {
 
 
 }
-.imovel-valor{
+
+.imovel-valor {
   margin-top: -30px;
   margin-bottom: 60px;
+  margin-left: 7px;
   font-family: 'Poppins', sans-serif;
-  
-}
-#coracao {
-    display: none;
-  }
+  font-weight: bold;
 
-  #coracao:checked + label > svg {
-    animation: preencher;
-    animation-duration: .5s;
-    animation-fill-mode: forwards;
-  }
-
-  @keyframes preencher {
-    0% {
-      fill: transparent;
-    }
-    100% {
-      fill: green;
-    }
-  }
-</style>
+}</style>
