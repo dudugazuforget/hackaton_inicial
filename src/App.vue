eeb7405152b9b4df81e4a332630252f7a603fab6
dudugazuforget <dudu99744@gmail.com>
2023-08-03 09:20:54 -0300

    atualização NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup>
 import { imoveis } from '@/_data/imoveis.js';
+import { ref } from 'vue';
 
+
+
+const ok = ref(false);
+const vdd = ref(true)
+function SobreNos (){
+ ok.value = true
+ vdd.value = false
+}
 
 
 </script>
@@ -8,16 +17,20 @@ import { imoveis } from '@/_data/imoveis.js';
 <template>
   <div class="title">
     <h1 class="name-title">IfCoders</h1>
-    <button class="sobre-nos">Sobre nós</button>
+    <button @click="SobreNos" class="sobre-nos">Sobre nós</button>
+    <div v-if="ok" class="sobre">
+      <p class="text-sobre">Somos a <span style="color:rgb(34, 161, 34) ;">IfCoders</span>, uma inovadora empresa de locação de imóveis fundada por dois jovens empreendedores programadores. Unimos tecnologia e expertise no mercado imobiliário para simplificar e aprimorar a experiência de locação. Nossa plataforma intuitiva e orientada por dados conecta proprietários e inquilinos de forma rápida e segura. Valorizamos o atendimento ao cliente e buscamos criar uma comunidade confiável e sustentável. Junte-se a nós nessa jornada rumo a uma locação de imóveis mais inteligente e gratificante. Bem-vindo à <span style="color:rgb(34, 161, 34) ;">IfCoders</span>!</p>
+      
+    </div>
     <button class="contato">Contato</button>
     <button class="anunciar">Anunciar</button>
 
   </div>
   <div class="navbar"></div>
-  <div class="listagem-imoveis">
+  <div class="listagem-imoveis" >
     <div v-for="imovel in imoveis" :key="imovel.id" class="card-casa">
       <img :src="imovel.img" alt="capa" class="imovel-capa">
-      <hr>
+      <hr >
       <span><button class="b-alugar">Alugar</button></span>
       <p class="imovel-valor">R${{ imovel.valor }}.000</p>
       <p class="imovel-local">{{ imovel.local }}</p>
@@ -71,6 +84,50 @@ import { imoveis } from '@/_data/imoveis.js';
   margin-top: -37px;
   margin-left: 230px;
 }
+
+.sobre{
+  position: absolute;
+  padding:20px ;
+  top: 95px;
+  width: 700px;
+  border-bottom-right-radius: 5px;
+  height: 280px;
+  color: rgb(73, 68, 68);
+  background-color: rgb(241, 235, 235);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation-name: ease;
+  animation-duration: 0.7s;
+}
+.text-sobre{
+  animation-name: slide;
+  animation-duration: 3s;
+}
+
+@keyframes slide{
+  from{
+    
+    opacity: 0;
+  }
+  to{
+    
+    opacity: 1;
+  }
+  
+}
+
+@keyframes ease{
+  from{
+    
+    width: 0px;
+  }
+  to{
+    
+    width: 700px;
+  }
+}
+
 
 .imovel-capa {
  
